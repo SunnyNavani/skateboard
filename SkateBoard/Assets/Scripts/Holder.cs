688b55cccdb777b5207b429b2ab24e4a2e064753
zeerox <zeerox@gmail.com>
2018-06-04 22:30:26 +0530

    first commit

---
+++ SkateBoard/Assets/Scripts/Holder.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holder : MonoBehaviour{
+
+
+	
+
+
+	public GameObject initSpawnPoint;
+	public GameObject[] platforms;
+	//public float delayTimer=1f;
+
+
+	//public CameraScript cam;
+	public float bounds;
+	float timer;
+	public float start, end;
+		
+	public Vector3 initPosition;
+	public GameObject gameObject1;
+	public static class WorldContext
+	{
+		public static float OffScreenX = 0.2f;
+		public static float OffScreenY = -14.9f;
+	}
+
+
+	private void Awake(){
+		initPosition = initSpawnPoint.transform.position;
+		
+		currentPositon = initPosition;
+//		initPosition = currentPositon = transform.position;
+	}
+	private void Start (){
+
+		//timer = delayTimer;	
+	}
+	void Update()
+	{
+		//timer -= Time.deltaTime;
+		//if (timer <= 0) {
+		//	timer = delayTimer;
+		if (transform.position.x < Camera.main.transform.position.x - WorldContext.OffScreenX) {
+			prev = Instantiate (platforms [Random.Range (0, platforms.Length)], currentPositon, Quaternion.identity, transform);
+			float minClear = prev.GetComponent<EdgeCollider2D> ().bounds.size.x;
+			float diff = Random.Range (minClear + 3f, minClear + 3f);
+			currentPositon = currentPositon + new Vector3 (diff, Random.Range (start, end), 0);
+			currentPositon.y = Mathf.Clamp (currentPositon.y, initPosition.y - bounds, initPosition.y + bounds);
+		} 
+		if (transform.position.x > Camera.main.transform.position.x - WorldContext.OffScreenX)
+			Destroy (gameObject1);
+
+}
+
+
+
+//	private void createBlock(){
+//
+//
+//	}
+	
+
+	
+	// Update is called once per frame
+//	private void Update (){
+
+		
+//		if (prev.transform.position.x <= cam.getRightBound() + cam.getCamLength()){
+//			createBlock();
+//		
+//		}
+//
+//		int childCount =  transform.childCount;
+//		for (int i = childCount-1; i >= 0; --i){
+//			var trans = transform.GetChild(i);
+//			if (trans.position.x+cam.getCamLength()*1.5f <= cam.transform.position.x){
+//				Destroy(trans.gameObject);
+//			}
+//			
+//		}
+		
+//	}
+	
+	private Vector3 currentPositon;
+	private GameObject prev;
+	private float camPos;
+
+
+}
---
+++ SkateBoard/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerControl : MonoBehaviour {
+
+//	bool move = false;
+	public Rigidbody2D rb;
+	public float speed =20f, jumpVelocity=10f,rotationSpeed=2f; 
+	float hinput=0;
+	public bool isGrounded=false;
+	private float turnDirection = 0.0f; 
+	Transform tagGround , myTrans;
+	public LayerMask playerMask;
+
+	public Button mybutton;
+	void Start()
+	{	rb = GetComponent<Rigidbody2D> ();
+		myTrans = this.transform;
+		tagGround = GameObject.Find (this.name + "/grounded").transform;
+	}
+
+
+	// Update is called once per frame
+//	public void Update () {
+//		if (Input.GetButtonDown ()) {
+//			move = true;
+//		}
+//		if (Input.GetButtonDown ()) {
+//			move = false;
+//		}
+		
+//	}
+	void Update()
+	{
+		//mybutton.onClick.AddListener (Flip);
+		if (!isGrounded ) {
+			Debug.Log ("rotateeeee");
+			//turnDirection = Input.GetAxis("Horizontal")*rotationSpeed;
+
+			rb.AddTorque (-rotationSpeed * Time.fixedDeltaTime * 10f, ForceMode2D.Force);
+		}
+	}
+	void FixedUpdate()
+	{
+//		if (move == true) {
+//			rb.AddForce (transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+//		}
+
+		isGrounded=Physics2D.Linecast(myTrans.position,tagGround.position,playerMask);
+
+		Move (hinput);
+
+
+			
+			
+
+	}
+	void Flip()
+	{
+			if (isGrounded ) {
+				Debug.Log ("rotateeeee");
+				//turnDirection = Input.GetAxis("Horizontal")*rotationSpeed;
+			transform.localScale+=new Vector3(-1,-1,-1);
+				//rb.AddTorque (-rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+			}
+	}
+	void Move(float horizontalInput)
+	{
+		Vector2 moveVal = rb.velocity;
+		moveVal.x = horizontalInput * speed;
+		rb.velocity = moveVal;
+	}
+	public void startMoving(float horizontalInput)
+	{
+		hinput = horizontalInput;
+		Debug.Log ("im here");
+
+	}
+	public void slowDownMoving(float horizontalInput)
+	{// Debug.Log ("hinput" + hinput);
+//		while (hinput >= 0) {
+//			Debug.Log ("hinputs = " + hinput);
+//			hinput = hinput - 1f;
+//		}
+////		hinput = 0;
+//		if (hinput <= 0)
+//		{
+			hinput = 2.2f;
+	//	rb.AddTorque (turnDirection * 0f * Time.fixedDeltaTime * 0f);
+		//	Debug.Log ("slowdown");
+		//}
+	}
+
+	public void jump()
+	{
+		if (isGrounded)
+			rb.velocity += jumpVelocity * Vector2.up;
+	}
+}
